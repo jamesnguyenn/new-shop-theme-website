02ae50a11f4dacc58665b1c0470f2af0b34d1cd6
unknown <nguyenquangthien151095@gmail.com>
2022-01-08 22:45:41 +0700

    change navbar-icon

--- js/app.js
+++ js/app.js
@@ -4,12 +4,18 @@ let $$ = document.querySelectorAll.bind(document);
 let stickyMenu = $(".sticky-menu");
 let iconSticky = $(".menu-sticky-logo");
 let linkMenu = $$(".menu-item");
+let menu = $(".menu");
 console.log(linkMenu);
 //Listener
 iconSticky.addEventListener("click", () => {
-  stickyMenu.classList.toggle("menu-active");
+  menu.classList.toggle("menu-active");
 });
-
+// iconSticky.addEventListener("click", () => {
+//   menu.classList.toggle("sticky-menu");
+// });
+iconSticky.addEventListener("click", () => {
+  iconSticky.classList.toggle("menu-sticky-cancel-active");
+});
 iconSticky.addEventListener("click", noScroll);
 
 linkMenu.forEach((element) => {
